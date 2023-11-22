bb3669e4d7bfb3d6bace2b9db39ae6afb4c0bad2
Valentin Baud <vabaud@c1r4p5.42lehavre.fr>
2023-11-22 13:18:16 +0100

    add

---
+++ GET_NEXT_LINE/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vabaud <vabaud@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/16 01:12:03 by vabaud            #+#    #+#             */
+/*   Updated: 2023/11/22 13:14:15 by vabaud           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif
